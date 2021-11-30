df9a52ceacd891bcc993314584cd227191e9afb3
Nicolas Froidure <nicolas.froidure@gmail.com>
2021-11-30 11:51:41 +0100

    feat(frontend): add some layout components
    fix #16 fix #17

--- front/components/title.tsx
+++ front/components/title.tsx
@@ -1,8 +1,23 @@
-import styles from '../components/title.module.css'
+import styles from "../components/title.module.css";
 
-
-export default function Title() {
-    return (
-        <h1 className={styles.h1}>Bienvenue sur PhoneCoop !</h1>
-    )
+export default function Title({
+  children,
+  level,
+}: {
+  children: React.ReactNode;
+  level: 1 | 2 | 3;
+}) {
+  return level === 1 ? (
+    <h1 className={styles.h1}>
+      {children}
+    </h1>
+  ) : level === 2 ? (
+    <h2 className={styles.h2}>
+      {children}
+    </h2>
+  ) : (
+    <h3 className={styles.h3}>
+      {children}
+    </h3>
+  );
 }
